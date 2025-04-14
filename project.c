05f8b69642810af14fe7559c9dc9710ddee9c286
OwOShiba <noahyap3@gmail.com>
2025-04-14 16:39:00 -0400

    alu, r&w register, pc

--- project.c
+++ project.c
@@ -5,14 +5,44 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-
+    switch (ALUControl)
+    {
+        case 0: 
+            *ALUresult = A + B; 
+            break;
+        case 1: 
+            *ALUresult = A - B; 
+            break;
+        case 2: 
+            *ALUresult = A < B ? 1 : 0; 
+            break; 
+        case 3: 
+            *ALUresult = ((int)A < (int)B) ? 1 : 0; 
+            break;
+        case 4: 
+            *ALUresult = A & B; 
+            break;
+        case 5: 
+            *ALUresult = A | B; 
+            break;
+        case 6: 
+            *ALUresult = B << 16; 
+            break;
+        case 7: 
+            *ALUresult = !A; 
+            break;
+        default: 
+            *ALUresult = 0; 
+            break;
+    }
+    *Zero = (*ALUresult == 0) ? 1 : 0;
 }
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+    
 }
 
 
@@ -36,7 +66,8 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    *data1 = Reg[r1];
+    *data2 = Reg[r2];
 }
 
 
@@ -66,13 +97,34 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
-
+    if(RegWrite==1){
+        if(MemtoReg==1){
+            if(RegDst==1){
+                Reg[r3]=memdata;
+            }else{
+                Reg[r2]=memdata;
+            }
+        }else{
+            if(RegDst==1){
+                Reg[r3]=ALUresult;
+            }else{
+                Reg[r2]=ALUresult;
+            }
+        }
+    }
 }
 
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
+    *PC=PC+4;
 
+    if(Jump==1){
+        *PC= (jsec<<2) | (*PC | 00);
+    }
+    else if(Branch==1 && Zero==1){
+        *PC += (extended_value<<2);
+    }
 }
 
